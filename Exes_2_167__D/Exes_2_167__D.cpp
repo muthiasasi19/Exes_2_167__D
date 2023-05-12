981da6c4cd0819fd90e83dce3b56d7ffbe1a1ce1
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-05-11 21:07:12 -0700

    gj

--- Exes_2_167__D/Exes_2_167__D.cpp
+++ Exes_2_167__D/Exes_2_167__D.cpp
@@ -1,27 +1,61 @@
 #include <iostream>
 using namespace std;
 
-int  i, number, median;
-int sasi[36], muthia, marsakina;
 
-void input();
+// input
+int main();
+int input()
 {
-	cout << "Berapa banyak elemen yang diinginkan ? :"; // mengeluarkan karakter yg berada pd tanda kutip ke layar
-	cin >> number;									 // memasukkkan nilai user pd program
+	int count, i, number, median;     //  number = angka yg di cari, count= jlmh elemen, i = indeks
+	int sasi[36], muthia, marsakina;
 
-	for (i = 0; i < sasi[36]; i++)						// menggunakan fungsi for (looping yg termasuk ke dlm looping yg dapat dihitung, i < count (akan terus dijalankan selama < count) i++ mrpkn post increment (setiap perulangan i akan bertambah 1)
+	cout << "Berapa banyak elemen yang diinginkan ? :"; // mengeluarkan karakter yg berada pd tanda kutip ke layar
+	cin >> count;									 // memasukkkan nilai user pd program
+
+	for (i = 0; i < count; i++) 					
 	{													// eksekusi program
 		cout << "Masukkan angka " << (i + 1) << ":";				// i+1 (i akan bertambah 1 sesuai dgn elemen yg diinginkan/  ingin menampilkan nomor angka yang diminta dimulai dari 1.)
 		cin >> number;									// nilai nilai yg dimasukkan oleh user
 	}
 
-	void BinarySearch();
-	{
-		cout <<"Masukkan angka yang ingin dicari:";
+	//  BinarySearch
+	void BinarySearch(); {
+		cout << "Masukkan angka yang ingin dicari:";
 		cin >> number;
 
-		marsakina = 0; // upperbound
-		muthia = count - 1;  // lowerbound
-		median = (first + last) / 2;
+
+		muthia = 0;								// upperbound
+		marsakina = number - 1;						 // lowerbound
+		median = (marsakina + muthia) / 2;
+		while (muthia <= marsakina)				// menunjukkan perulangan yg tdk dpt dihitung karena membutuhkan kondisi utk looping bs berhenti (selama muthia <= marsakina)
+		{
+			if (sasi[median] < number) // statemen dengan satu kondisi
+			{
+				muthia = median + 1;
+
+			}
+			else if (sasi[median] == number) //  menggunakan if else if else karena >2 kondisi (arr[middle] lebih kecil dari, sama dengan, atau lebih besar dari number)
+			{
+				cout << number << " ditemukan dalam array pada lokasi ke " << median + 1 << "\n"; // angka yang dicari (number) ditemukan dalam array pada lokasi (median + 1)
+				break;
+			}
+			else
+			{
+				marsakina = median - 1;
+			}
+			median = (muthia + marsakina) / 2;
+			{
+
+				if (muthia > marsakina)
+				{
+					cout << number << " tidak ditemukan dalam array";
+				}
+				return 0;
+			}
+
+			int main(); {
+		
+				BinarySearch();
+			}
+		}
 	}
-}
