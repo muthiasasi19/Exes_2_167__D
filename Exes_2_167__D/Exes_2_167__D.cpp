4a0dfc7d540d79ed6126f8f415507634e56e04f0
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-05-11 19:39:13 -0700

    Add project files.

---
+++ Exes_2_167__D/Exes_2_167__D.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int  i, number, median;
+int sasi[36], muthia, marsakina;
+
+void input();
+{
+	cout << "Berapa banyak elemen yang diinginkan ? :"; // mengeluarkan karakter yg berada pd tanda kutip ke layar
+	cin >> number;									 // memasukkkan nilai user pd program
+
+	for (i = 0; i < sasi[36]; i++)						// menggunakan fungsi for (looping yg termasuk ke dlm looping yg dapat dihitung, i < count (akan terus dijalankan selama < count) i++ mrpkn post increment (setiap perulangan i akan bertambah 1)
+	{													// eksekusi program
+		cout << "Masukkan angka " << (i + 1) << ":";				// i+1 (i akan bertambah 1 sesuai dgn elemen yg diinginkan/  ingin menampilkan nomor angka yang diminta dimulai dari 1.)
+		cin >> number;									// nilai nilai yg dimasukkan oleh user
+	}
+
+	void BinarySearch();
+	{
+		cout <<"Masukkan angka yang ingin dicari:";
+		cin >> number;
+
+		marsakina = 0; // upperbound
+		muthia = count - 1;  // lowerbound
+		median = (first + last) / 2;
+	}
+}
